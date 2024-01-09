7a7b63b5332718e716b90f04fc5225801b923f45
vvalvvizal <vvalvvizal@gmail.com>
2024-01-09 16:36:29 +0900

    Initial commit

---
+++ todolist/android/app/src/main/kotlin/com/doji/todolist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.doji.todolist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ todolist/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title:Text('나의 첫 앱'),), body:Text('hello world!'),);
+  }
+}
+
+
