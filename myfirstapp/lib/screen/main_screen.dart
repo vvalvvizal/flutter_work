3031b2cf758111d05fff471cdc59e6ebffb98acd
vvalvvizal <vvalvvizal@gmail.com>
2024-01-29 14:23:57 +0900

    fix : logo color

--- myfirstapp/lib/screen/main_screen.dart
+++ myfirstapp/lib/screen/main_screen.dart
@@ -9,23 +9,38 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   @override
-  List lstHello = ['도연', '지훈', '보리', '몽실', '머핀'];
+  String msg = '이 곳에 입력값이 업데이트됩니다.';
+  TextEditingController idController = TextEditingController();
+  //TextField 컨트롤러
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('메인화면'),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          //ListView 목록형 위젯
-          //아이템 UI를 return문에 구성
-          return ListTile(
-            title: Text('${lstHello[index]}'),
-            subtitle: Text('서브타이틀'),
-          );
-        },
-        itemCount: lstHello.length /*ListView의 길이 지정*/,
+      body: Column(
+        children: [
+          TextField(
+            controller: idController,
+            decoration: InputDecoration(labelText: '아이디를 입력해주세요'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              //클릭시 동작
+              //setState 밖에서 업데이트도 가능, 전체 다시 빌드
+              setState(() {
+                //widget update
+                //setState는 특정 위젯 별도로 업데이트
+                msg = idController.text.toString();
+              });
+            },
+            child: Text('아이디 입력 값 가져오기'),
+          ),
+          Text(
+            msg,
+            style: TextStyle(fontSize: 30),
+          )
+        ],
       ),
     );
   }
